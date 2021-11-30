75e35f15f6dfd1453c858b15202f7d396d998b5b
문의현 <71213079+euihyeonmoon@users.noreply.github.com>
2021-11-30 17:14:30 +0900

    Update main.py

--- ver2/main.py
+++ ver2/main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     try:
         exec(open("newscrawl.py").read())
         exec(open("json_to_frame.py").read())
-        #exec(open("Preprocessor.py").read())
-        #exec(open("scoring.py").read())
+        exec(open("Preprocessor.py").read())
+        exec(open("scoring.py").read())
         print("All process is done")
 
     except:
